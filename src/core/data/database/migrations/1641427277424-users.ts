3a85d89fb6a1885268afef7d77aa18ed8f965446
ThiagoRaposo <thiago@ciclix.com>
2022-01-05 21:08:34 -0300

    added users/mensagens migrations and user/mensagem entities

---
+++ src/core/data/database/migrations/1641427277424-users.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class users1641427277424 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
