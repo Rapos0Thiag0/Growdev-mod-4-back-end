afd9fc7f42215ca98fc42954aafab35d1425e16e
ThiagoRaposo <thiago@ciclix.com>
2022-01-10 02:20:16 -0300

    orm test

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,10 @@ module.exports = {
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
-  entities: ["src/core/data/database/entities/**/*.js"],
+  entities: [
+    "src/core/data/database/entities/**/*.js",
+    // "dist/core/data/database/entities/**/*.entity.js",
+  ],
   migrations: ["src/core/data/database/migrations/**/*.js"],
   cli: {
     entitiesDir: "src/core/data/database/entities",
